3356b3418d277b88fe96d70ca7852ba366ffb8be
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2024-01-13 17:45:57 -0800

    Added SideBarComponent

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ProjectSideBar from './components/ProjectSideBar';
+
+function App() {
+  return (
+    <>
+      <main className='h-screen my-8'>
+        <ProjectSideBar />
+      </main>
+    </>
+  );
+}
+
+export default App;
